d2eabf88b8442cd1286fc987660666fab5a95b62
Sudipa Acharya <sudipaacharya351@gmail.com>
2022-12-07 13:57:03 +0530

    code added

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,5 +1,18 @@
 // import using require
+var Shape = require('./shape.js');
 
+// declare class
+class Circle extends Shape {
+    constructor(color) {
+         super();
+    }
+    calculateArea() {
+
+    }
+}
+
+// export class using module.exports
+module.exports = Circle;
 // declare class
 
 
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
+class Shape {
+    constructor() {
+         this.color = "green";
+    }
+    drawShape() {
 
+    }
+    calculateArea() {
+
+    }
+}
+
+// export class using module.exports
+module.exports = Shape;
 // export class using module.exports
